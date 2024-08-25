aa49bf0c2ebbf9f3919792d256cfadf9421a7a4e
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-25 13:58:22 +0300

    Create user validator implementation

---
+++ src/main/java/dkcorp/post_service/validator/user/UserValidatorImpl.java
@@ -0,0 +1,25 @@
+package dkcorp.post_service.validator.user;
+
+import dkcorp.post_service.client.UserServiceClient;
+import dkcorp.post_service.dto.UserDto;
+import dkcorp.post_service.exception.NotFoundException;
+import feign.FeignException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserValidatorImpl implements UserValidator {
+    private final UserServiceClient userService;
+
+    @Override
+    public void validateUserExistence(Long userId) {
+        try {
+            UserDto userDto = userService.getUserById(userId);
+        } catch (FeignException.NotFound ex) {
+            throw new NotFoundException(String.format("User with id '%d' not found", userId));
+        } catch (FeignException ex) {
+            throw new RuntimeException("Error fetching user", ex);
+        }
+    }
+}
