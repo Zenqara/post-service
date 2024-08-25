46b99ecb22524fce0f3b8df06001d5084f6c0ede
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-25 14:00:29 +0300

    Add enable feign clients annotation

--- src/main/java/dkcorp/post_service/PostServiceApplication.java
+++ src/main/java/dkcorp/post_service/PostServiceApplication.java
@@ -2,12 +2,14 @@ package dkcorp.post_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class PostServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PostServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PostServiceApplication.class, args);
+    }
 
 }
