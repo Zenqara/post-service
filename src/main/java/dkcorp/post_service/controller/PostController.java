99030130cca9388e900247400c98fb5f9f91d247
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-26 12:31:58 +0300

    Use rest controller annotation instead of controller

--- src/main/java/dkcorp/post_service/controller/PostController.java
+++ src/main/java/dkcorp/post_service/controller/PostController.java
@@ -9,7 +9,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +18,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Validated
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/posts")
 public class PostController {
