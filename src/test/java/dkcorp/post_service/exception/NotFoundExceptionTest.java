a2eb0a847f342e2e725d9953e80d2051b9c66b9c
Denis Karpov <karpovdenis.al@gmail.com>
2024-08-26 14:33:52 +0300

    Create tests for not found exc

---
+++ src/test/java/dkcorp/post_service/exception/NotFoundExceptionTest.java
@@ -0,0 +1,26 @@
+package dkcorp.post_service.exception;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class NotFoundExceptionTest {
+    @Test
+    void testExceptionMessage() {
+        String errorMessage = "Entity not found";
+        NotFoundException exception = new NotFoundException(errorMessage);
+
+        assertThat(exception.getMessage()).isEqualTo(errorMessage);
+    }
+
+    @Test
+    void testExceptionThrown() {
+        String errorMessage = "Entity not found";
+
+        assertThatThrownBy(() -> {
+            throw new NotFoundException(errorMessage);
+        }).isInstanceOf(NotFoundException.class)
+                .hasMessage(errorMessage);
+    }
+}
