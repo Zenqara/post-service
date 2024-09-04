2433755e826450cf9936a01f8b544cef44d76906
Denis Karpov <karpovdenis.al@gmail.com>
2024-09-04 18:48:11 +0300

    Commit working producer config

--- src/main/java/dkcorp/post_service/config/kafka/KafkaProducerConfig.java
+++ src/main/java/dkcorp/post_service/config/kafka/KafkaProducerConfig.java
@@ -1,34 +1,35 @@
 package dkcorp.post_service.config.kafka;
 
+import dkcorp.post_service.event.TestEvent;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-public class ProducerConfig {
+public class KafkaProducerConfig {
     @Bean
-    public ProducerFactory<String, String> producerFactory() {
+    public ProducerFactory<String, TestEvent> producerFactory() {
         return new DefaultKafkaProducerFactory<>(senderProps());
     }
 
     private Map<String, Object> senderProps() {
         Map<String, Object> props = new HashMap<>();
-        props.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
-        props.put(org.apache.kafka.clients.producer.ProducerConfig.LINGER_MS_CONFIG, 10);
-        props.put(org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return props;
     }
 
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
+    public KafkaTemplate<String, TestEvent> kafkaTemplate(ProducerFactory<String, TestEvent> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
     }
 }
