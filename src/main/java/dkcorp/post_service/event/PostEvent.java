2ae39b6d3a2b280b14cabec7f472d07b8b051ace
Denis Karpov <karpovdenis.al@gmail.com>
2024-09-07 09:38:34 +0300

    Add NoArgsConstructor and AllArgsConstructor annotations

--- src/main/java/dkcorp/post_service/event/PostEvent.java
+++ src/main/java/dkcorp/post_service/event/PostEvent.java
@@ -1,12 +1,16 @@
 package dkcorp.post_service.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostEvent {
     private Long postId;
     private Long actorId;
