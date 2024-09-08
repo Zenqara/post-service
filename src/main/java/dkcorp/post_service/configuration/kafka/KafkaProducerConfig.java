0f3cab27933a6cef98582452be35362761118a5b
Denis Karpov <karpovdenis.al@gmail.com>
2024-09-08 14:10:19 +0300

    Use KafkaProducerProperties class

--- src/main/java/dkcorp/post_service/configuration/kafka/KafkaProducerConfig.java
+++ src/main/java/dkcorp/post_service/configuration/kafka/KafkaProducerConfig.java
@@ -1,7 +1,7 @@
-package dkcorp.post_service.config.kafka;
+package dkcorp.post_service.configuration.kafka;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -13,7 +13,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@RequiredArgsConstructor
 public class KafkaProducerConfig {
+    private final KafkaProducerProperties properties;
+
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         return new DefaultKafkaProducerFactory<>(producerConfigs());
@@ -26,10 +29,12 @@ public class KafkaProducerConfig {
 
     private Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093,localhost:9094");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, properties.getKeySerializer());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, properties.getValueSerializer());
+        props.put(ProducerConfig.RETRIES_CONFIG, properties.getRetries());
+        props.put(ProducerConfig.ACKS_CONFIG, properties.getAcks());
+        props.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, properties.isAddTypeHeaders());
         return props;
     }
 }
